1d7337d40745efc96780e6b56409c0fdc4ed253c
Adrian <adrianburgio@gmail.com>
2021-01-02 12:17:37 -0300

    Listar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState , useEffect} from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+
 
 function App() {
+
+  const [usuario, setUsuario] = useState([])
+
+  useEffect(() => {
+    obtenerDatos()
+  }, [])
+
+  const obtenerDatos = async () => {
+    const data = await fetch('https://localhost:44358/api/usuarios')
+    const usuarios = await data.json()
+    setUsuario(usuarios)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2 className="mt-5">Usuarios</h2>
+      <table className="table table-striped table-hover ">
+       <thead className="bg-secondary">
+          <tr>
+            <th>Nombre de usuario</th>
+            <th>Email</th>
+            <th>Teléfono</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+        {
+          usuario.map(item => (
+          <tr key={item.id}>
+            <td>{item.username}</td>
+            <td>{item.email}</td>
+            <td>{item.telefono}</td>
+            <td>
+              <button type="button" className="btn btn-basic text-primary" data-toggle="tooltip" title="Editar"><FontAwesomeIcon icon={faEdit}/></button>
+              <button type="button" className="btn btn-basic text-danger" data-toggle="tooltip" title="Eliminar"><FontAwesomeIcon icon={faTrashAlt}/></button>
+            </td>
+          </tr>
+          ))
+        }
+        </tbody>
+      </table> 
     </div>
   );
 }
